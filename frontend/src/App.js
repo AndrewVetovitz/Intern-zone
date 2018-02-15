d3a4cbbb67130c4a72dcc2d24a78ae415bf1f03d
AndrewVetovitz <AndrewVetovitz@gmail.com>
2018-02-15 16:12:39 -0500

    fixed testing build issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Navbar from './components/Navbar/navbar';
 import Home from './components/Home/home';
 
-import Reboot from 'material-ui/es/Reboot/Reboot';
+import Reboot from 'material-ui/Reboot';
 
 class App extends Component {
     render() {
--- frontend/src/components/Tile/tile.js
+++ frontend/src/components/Tile/tile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import { withStyles } from 'material-ui/styles';
 import Card, { CardContent } from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
