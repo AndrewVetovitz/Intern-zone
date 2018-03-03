cb87f636d0c5bfbe1c63462f6b43f030358fa69e
AndrewVetovitz <AndrewVetovitz@gmail.com>
2018-03-03 01:58:09 -0500

    added companies and reducer files to redux directory

---
+++ frontend/src/redux/modules/companies.ts
@@ -0,0 +1,11 @@
+const LOAD = 'intern-zone/companies/LOAD';
+const LOAD_SUCCESSFUL = 'intern-zone/companies/LOAD_SUCCESFUL';
+const LOAD_FAILURE = 'intern-zone/companies/LOAD_FAILURE';
+
+const initialState = {
+    loaded: false
+}
+
+export default function reducer(state: object = {}, action: object = {}) {
+
+}
