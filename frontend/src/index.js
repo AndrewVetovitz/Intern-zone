9a5b70860ea69787a67b8ada7137c4284b3972c9
AndrewVetovitz <AndrewVetovitz@gmail.com>
2018-03-04 00:44:06 -0500

    fixed index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+
 import App from './components/App/App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { Provider } from 'react-redux';
+import store from './store';
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+  registerServiceWorker();
