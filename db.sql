7437ee39dfa4a1d1ee06604b72796c90a28347c6
Andrew Vetovitz <AndrewVetovitz@gmail.com>
2018-11-20 18:20:35 -0500

    added anther table, fixed syntax issues for mysql

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE User (
+CREATE TABLE user (
     id INT  NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     email VARCHAR(256) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE User (
 
 CREATE TABLE company (
     id INT NOT NULL AUTO_INCREMENT,
-    clicks UNSIGNED INT DEFAULT 0,
+    clicks INT UNSIGNED DEFAULT 0,
     name VARCHAR(64) NOT NULL,
     description VARCHAR(2048) NOT NULL,
     website_link VARCHAR(512) NOT NULL,
@@ -17,22 +17,23 @@ CREATE TABLE company (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE position (
+CREATE TABLE job_position (
     id INT NOT NULL AUTO_INCREMENT,
     company_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     link VARCHAR(512),
     location VARCHAR(64),
+    open_status BOOLEAN DEFAULT 0,
     FOREIGN KEY (company_id) REFERENCES company(company_id),
     PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE position_history (
+CREATE TABLE job_position_history (
     id INT NOT NULL AUTO_INCREMENT,
     position_id INT NOT NULL,
     year INT NOT NULL,
     open_date DATE,
     close_date DATE,
-    FOREIGN KEY (position_id) REFERENCES position(position_id)
+    FOREIGN KEY (position_id) REFERENCES job_position(position_id),
     PRIMARY KEY(id)
-)
+);
